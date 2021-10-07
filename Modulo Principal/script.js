380cd62fbdc0f4ae2753b8b763ae344a10b3e59d
Nataly <90075773+NatxCS@users.noreply.github.com>
2021-10-07 14:22:30 -0500

    Add files via upload

--- Modulo Principal/script.js
+++ Modulo Principal/script.js
@@ -1,38 +1,168 @@
-var app = {
-    settings: {
-      container: $('.calendar'),
-      calendar: $('.front'),
-      days: $('.weeks span'),
-      form: $('.back'),
-      input: $('.back input'),
-      buttons: $('.back button')
-    },
-  
-    init: function() {
-      instance = this;
-      settings = this.settings;
-      this.bindUIActions();
-    },
-  
-    swap: function(currentSide, desiredSide) {
-      settings.container.toggleClass('flip');
-  
-      currentSide.fadeOut(900);
-      currentSide.hide();
-      desiredSide.show();
-  
-    },
-  
-    bindUIActions: function() {
-      settings.days.on('click', function(){
-        instance.swap(settings.calendar, settings.form);
-        settings.input.focus();
-      });
-  
-      settings.buttons.on('click', function(){
-        instance.swap(settings.form, settings.calendar);
-      });
+var meses = [
+    'Enero',
+    'Febrerp',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+];
+
+var dias = [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+    'Sabado'
+];
+
+var dias_abr = [
+    'Dom',
+    'Lun',
+    'Mar',
+    'Mie',
+    'Jue',
+    'Vie',
+    'Sab'
+];
+
+Number.prototype.pad = function(num) {
+    var str = '';
+    for(var i = 0; i < (num-this.toString().length); i++)
+        str += '0';
+    return str += this.toString();
+}
+
+function calendari(widget, data)
+{
+
+    var original = widget.getElementsByClassName('actiu')[0];
+
+    if(typeof original === 'undefined')
+    {
+        original = document.createElement('table');
+        original.setAttribute('data-actual',
+			      data.getFullYear() + '/' +
+			      data.getMonth().pad(2) + '/' +
+			      data.getDate().pad(2))
+        widget.appendChild(original);
     }
-  }
-  
-  app.init();
+
+    var diff = data - new Date(original.getAttribute('data-actual'));
+
+    diff = new Date(diff).getMonth();
+
+    var e = document.createElement('table');
+
+    e.className = diff  === 0 ? 'amagat-esquerra' : 'amagat-dreta';
+    e.innerHTML = '';
+
+    widget.appendChild(e);
+
+    e.setAttribute('data-actual',
+                   data.getFullYear() + '/' +
+                   data.getMonth().pad(2) + '/' +
+                   data.getDate().pad(2))
+
+    var fila = document.createElement('tr');
+    var titulo = document.createElement('th');
+    titulo.setAttribute('colspan', 7);
+
+    var boto_prev = document.createElement('button');
+    boto_prev.className = 'boto-prev';
+    boto_prev.innerHTML = '&#9666;';
+
+    var boto_next = document.createElement('button');
+    boto_next.className = 'boto-next';
+    boto_next.innerHTML = '&#9656;';
+
+    titulo.appendChild(boto_prev);
+    titulo.appendChild(document.createElement('span')).innerHTML = 
+        meses[data.getMonth()] + '<span class="any">' + data.getFullYear() + '</span>';
+
+    titulo.appendChild(boto_next);
+
+    boto_prev.onclick = function() {
+        data.setMonth(data.getMonth() - 1);
+        calendari(widget, data);
+    };
+
+    boto_next.onclick = function() {
+        data.setMonth(data.getMonth() + 1);
+        calendari(widget, data);
+    };
+
+    fila.appendChild(titulo);
+    e.appendChild(fila);
+
+    fila = document.createElement('tr');
+
+    for(var i = 1; i < 7; i++)
+    {
+        fila.innerHTML += '<th>' + dias_abr[i] + '</th>';
+    }
+
+    fila.innerHTML += '<th>' + dias_abr[0] + '</th>';
+    e.appendChild(fila);
+
+    
+    var inici_mes =
+        new Date(data.getFullYear(), data.getMonth(), -1).getDay();
+
+    var actual = new Date(data.getFullYear(),
+			  data.getMonth(),
+			  -inici_mes);
+
+    
+    for(var s = 0; s < 6; s++)
+    {
+        var fila = document.createElement('tr');
+
+        for(var d = 1; d < 8; d++)
+        {
+	    var cela = document.createElement('td');
+	    var span = document.createElement('span');
+
+	    cela.appendChild(span);
+
+            span.innerHTML = actual.getDate();
+
+            if(actual.getMonth() !== data.getMonth())
+                cela.className = 'fora';
+
+            
+            if(data.getDate() == actual.getDate() &&
+	       data.getMonth() == actual.getMonth())
+		cela.className = 'avui';
+
+	    actual.setDate(actual.getDate()+1);
+            fila.appendChild(cela);
+        }
+
+        e.appendChild(fila);
+    }
+
+    setTimeout(function() {
+        e.className = 'actiu';
+        original.className +=
+        diff === 0 ? ' amagat-dreta' : ' amagat-esquerra';
+    }, 20);
+
+    original.className = 'inactiu';
+
+    setTimeout(function() {
+        var inactius = document.getElementsByClassName('inactiu');
+        for(var i = 0; i < inactius.length; i++)
+            widget.removeChild(inactius[i]);
+    }, 1000);
+
+}
+
+calendari(document.getElementById('calendari'), new Date());
